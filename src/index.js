5a4f1ba6363ad57f4f73f7e7925e36c1d045a928
Alexander Kostin <asurv2@gmail.com>
2022-06-30 18:40:19 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import 'modern-normalize/modern-normalize.css';
 import './index.css';
+import App from './components/App';
+import { Provider } from 'react-redux';
+import { store } from './Redux/store';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <App />
+        </Provider>
+    </React.StrictMode>
 );
