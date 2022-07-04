d2df206c0c6cb439ab939b56ddcb46b37e266dc5
Alexander Kostin <asurv2@gmail.com>
2022-07-04 23:36:47 +0300

    Update SignIn.jsx

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -1,23 +1,27 @@
 import * as React from 'react';
-import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
-import CssBaseline from '@mui/material/CssBaseline';
-import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
-import Typography from '@mui/material/Typography';
-import Container from '@mui/material/Container';
-import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Link } from 'react-router-dom';
-import { routesPaths } from 'routerSettings/routesPaths';
 import { useNavigate } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import {
+  Box,
+  Grid,
+  Avatar,
+  Button,
+  Checkbox,
+  Container,
+  TextField,
+  Typography,
+  createTheme,
+  CssBaseline,
+  ThemeProvider,
+  FormControlLabel,
+} from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import { loginUser } from 'redux/operations/operations-user';
 import selectors from 'redux/selectors';
 import { LoaderButton } from 'components/LoaderButton';
+import { routesPaths } from 'routerSettings/routesPaths';
+
 var CryptoJS = require('crypto-js');
 
 const theme = createTheme();
