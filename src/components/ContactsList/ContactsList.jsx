ba020a7cc1e9d97b1864d2952335ab544a143825
Alexander Kostin <asurv2@gmail.com>
2022-07-04 21:15:12 +0300

    Update ContactsList.jsx

--- src/components/ContactsList/ContactsList.jsx
+++ src/components/ContactsList/ContactsList.jsx
@@ -2,7 +2,7 @@ import { ContactsItem } from './ContactsItem';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchContacts } from 'redux/operations/operations-contacts';
-import { setFilterValue } from 'redux/slice/сontactsSlice';
+import { setFilterValue } from '../../redux/slice/contactsSlice';
 import { toast } from 'react-toastify';
 import selectors from 'redux/selectors';
 import LoaderPage from 'components/LoaderPage';
