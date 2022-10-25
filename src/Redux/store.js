c05f2c40495bbf709415f754e50db65400b7962e
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-10-25 12:34:05 +0300

    refactoring

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,35 +1,42 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
 import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import userSlice from '../redux/slice/userSlice';
-import contactsSlice from '../redux/slice/contactsSlice';
+    persistStore,
+    persistReducer,
+    PAUSE,
+    PURGE,
+    FLUSH,
+    PERSIST,
+    REGISTER,
+    REHYDRATE,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import userSlice from "../redux/slice/userSlice";
+import contactsSlice from "../redux/slice/contactsSlice";
 
 const persistConfig = {
-  key: 'auth',
-  storage,
-  whitelist: ['token'],
+    key: "auth",
+    storage,
+    whitelist: ["token"],
 };
 
 export const store = configureStore({
-  reducer: {
-    auth: persistReducer(persistConfig, userSlice),
-    contacts: contactsSlice,
-  },
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
-  // devTools: process.env.NODE_ENV !== 'production',
+    reducer: {
+        auth: persistReducer(persistConfig, userSlice),
+        contacts: contactsSlice,
+    },
+    middleware: (getDefaultMiddleware) =>
+        getDefaultMiddleware({
+            serializableCheck: {
+                ignoredActions: [
+                    FLUSH,
+                    REHYDRATE,
+                    PAUSE,
+                    PERSIST,
+                    PURGE,
+                    REGISTER,
+                ],
+            },
+        }),
+    // devTools: process.env.NODE_ENV !== 'production',
 });
 export const persistor = persistStore(store);
