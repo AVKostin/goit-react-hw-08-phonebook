c654611ac92751d16e899f2976f4492744fb640e
Alexander Kostin - Александр Костин <90350582+AVKostin@users.noreply.github.com>
2022-10-25 12:33:59 +0300

    refactoring

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,36 +1,38 @@
-import { createSelector } from '@reduxjs/toolkit';
+import { createSelector } from "@reduxjs/toolkit";
 
-const getIsAuth = state => state.auth.isAuth;
-const getIsLoading = state => state.auth.isLoading;
-const getAuthStatus = state => state.auth.status;
-const getToken = state => state.auth.token;
-const getUserName = state => state.auth.user.name;
-const getUserEmail = state => state.auth.user.email;
-const getFilterValue = state => state.contacts.filter;
-const getContactsStatus = state => state.contacts.status;
-const getContacts = state => state.contacts.items;
+const getIsAuth = (state) => state.auth.isAuth;
+const getIsLoading = (state) => state.auth.isLoading;
+const getAuthStatus = (state) => state.auth.status;
+const getToken = (state) => state.auth.token;
+const getUserName = (state) => state.auth.user.name;
+const getUserEmail = (state) => state.auth.user.email;
+const getFilterValue = (state) => state.contacts.filter;
+const getContactsStatus = (state) => state.contacts.status;
+const getContacts = (state) => state.contacts.items;
 
 const getFilteredContacts = createSelector(
-  [getContacts, getFilterValue],
-  (contacts, filter) => {
-    return contacts.filter(
-      contact =>
-        contact.name.toLowerCase().includes(filter.toLowerCase().trim()) ||
-        contact.number.includes(filter.toLowerCase().trim())
-    );
-  }
+    [getContacts, getFilterValue],
+    (contacts, filter) => {
+        return contacts.filter(
+            (contact) =>
+                contact.name
+                    .toLowerCase()
+                    .includes(filter.toLowerCase().trim()) ||
+                contact.number.includes(filter.toLowerCase().trim()),
+        );
+    },
 );
 
 const selectors = {
-  getFilteredContacts,
-  getContacts,
-  getContactsStatus,
-  getFilterValue,
-  getAuthStatus,
-  getIsAuth,
-  getIsLoading,
-  getToken,
-  getUserName,
-  getUserEmail,
+    getIsAuth,
+    getIsLoading,
+    getAuthStatus,
+    getToken,
+    getUserName,
+    getUserEmail,
+    getContactsStatus,
+    getFilterValue,
+    getContacts,
+    getFilteredContacts,
 };
 export default selectors;
